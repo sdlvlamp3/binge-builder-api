776ba7bdf6e30519cb0483d19755ee5b2853ef50
sdlvlamp3 <sdlviola@gmail.com>
2024-03-07 19:11:38 -0600

    Lots of fixes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+    
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: { error: 'Unable to create user.' }
+        end
+    end
+
+    def update
+        user = User.find(params[:id])
+        if user.udpate(user_params)
+             render json: user
+        else
+            render json: { error: 'Unable to update user.' }
+        end
+    end
+
+    def destroy
+        user = User.find(params[:id])
+        if user.destroy
+            render json: { message: 'Successfully deleted user.' }
+        else
+            render json: { error: 'Unable to delete user.' }
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :users
-  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
